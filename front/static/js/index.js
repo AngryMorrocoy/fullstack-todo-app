408ac19b244fc1886c763d9c2acab720cead3565
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-07-28 12:02:23 -0400

    Editing and deleting todos from frontend

--- front/static/js/index.js
+++ front/static/js/index.js
@@ -8,14 +8,28 @@ async function getTodos() {
 }
 
 function handleTodoListClick(target) {
-  if (target.type !== 'checkbox') return;
-  const todoID = target.parentElement.id;
+  const todoID = target.parentElement.id.replace('todo-', '');
+  if (target.type === 'checkbox') {
+    updateTodo(todoID, target.checked);
+  } else if (target.classList.contains('delete-todo')) {
+    deleteTodo(todoID);
+  }
+}
 
+function deleteTodo(todoID) {
+  const url = `${basePath}/api/todos/${todoID}/`;
+
+  axios
+    .delete(url)
+    .then(() => document.querySelector(`#todo-${todoID}`).remove());
+}
+
+function updateTodo(todoID, newCompleted) {
   const url = `${basePath}/api/todos/${todoID}/`;
 
   axios
     .patch(url, {
-      completed: target.checked,
+      completed: newCompleted,
     })
-    .then(console.log);
+    .then();
 }
