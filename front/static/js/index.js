eeb9b9832c7b15b74eb184332935439bf5c693b7
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-07-29 10:09:59 -0400

    Moved all the todos code to a store, more reusable haha

--- front/static/js/index.js
+++ front/static/js/index.js
@@ -5,32 +5,44 @@ async function getTodos() {
   return response.data;
 }
 
-function handleTodoListClick(target, onSuccess) {
-  if (target.parentElement.classList.contains('delete-todo'))
-    target = target.parentElement;
+const todoMethods = {
+  edit: updateTodo,
+  delete: deleteTodo,
+};
 
-  const todoID = target.parentElement.id.replace('todo-', '');
-  if (target.type === 'checkbox') {
-    updateTodo(todoID, target.checked, onSuccess);
-  } else if (target.classList.contains('delete-todo')) {
-    deleteTodo(todoID, onSuccess);
-  }
+function handleTodoListClick(target) {
+  const method = target.getAttribute('todo-method');
+  if (!method) return;
+
+  const todoID = Number(target.getAttribute('todo-id'));
+  const completed = target.checked;
+
+  todoMethods[method]({ todoID, completed });
 }
 
-function deleteTodo(todoID, onSuccess) {
+function deleteTodo({ todoID }) {
   const url = `${basePath}/api/todos/${todoID}/`;
 
-  axios.delete(url).then(onSuccess);
+  axios.delete(url).then(() => {
+    const todoStore = Alpine.store('todos');
+    todoStore.todos = todoStore.todos.filter((todo) => todo.id !== todoID);
+  });
 }
 
-function updateTodo(todoID, completed, onSuccess) {
+function updateTodo({ todoID, completed }) {
   const url = `${basePath}/api/todos/${todoID}/`;
 
   axios
     .patch(url, {
       completed,
     })
-    .then(onSuccess);
+    .then((res) => {
+      const todoStore = Alpine.store('todos');
+      todoStore.todos = todoStore.todos.map((todo) => {
+        if (todo.id === todoID) return res.data;
+        return todo;
+      });
+    });
 }
 
 function createTodo({ title, completed }, onSuccess) {
@@ -39,6 +51,37 @@ function createTodo({ title, completed }, onSuccess) {
 
   axios
     .post(url, { title, completed, owner })
-    .then(onSuccess)
+    .then((res) => {
+      onSuccess();
+      Alpine.store('todos').todos.push(res.data);
+    })
     .catch(console.log);
 }
+
+document.addEventListener('alpine:init', () => {
+  Alpine.store('todos', {
+    async init() {
+      this.todos = await getTodos();
+    },
+    filter: '',
+    todos: [],
+    get filterTodos() {
+      if (this.filter === 'all') return this.todos;
+      return this.filter === 'active' ? this.leftTodos : this.completedTodos;
+    },
+    get leftTodos() {
+      return this.todos.filter((todo) => !todo.completed);
+    },
+    get completedTodos() {
+      return this.todos.filter((todo) => todo.completed);
+    },
+  });
+
+  Alpine.store('darkmode', {
+    on: window.localStorage.getItem('theme') || true,
+    toggle() {
+      this.on = !this.on;
+      window.localStorage.setItem('dark-theme', this.on);
+    },
+  });
+});
