6e32ba036292ae0355373db64bc27d2f81f80ad0
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-07-30 19:18:05 -0400

    A

--- core/settings.py
+++ core/settings.py
@@ -115,7 +115,6 @@ DATABASES = {
 if "DATABASE_URL" in os.environ:
     # Configure Django for DATABASE_URL environment variable.
     DATABASES["default"] = dj_database_url.config(
-        os.environ.get("DATABASE_URL"),
         conn_max_age=MAX_CONN_AGE,
         ssl_require=True,
     )
